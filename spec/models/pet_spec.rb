924d1c6df68e4c9e7c3d77381edf50cff4af7124
David Atkins <atki1080@gmail.com>
2020-01-29 22:47:23 -0700

    Add pets model test

---
+++ spec/models/pet_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Pet, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :image_path}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :approximate_age}
+    it {should validate_presence_of :sex}
+  end
+  describe "relationships" do
+    it {should belong_to :shelter}
+  end
+  describe "methods" do
+  end
+end

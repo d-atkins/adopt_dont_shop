e3bd73bcfd72b1064441b8d43a2504af73200521
David Atkins <atki1080@gmail.com>
2020-01-30 16:11:59 -0700

    Update test name

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "from the shelters index page" do
-  it "I can fill the form and click submit" do
+  it "I can click the new shelter link, fill the form, and click submit" do
     Shelter.create!(name: "Shelter A", address: "123 Fake St", city: "Las Vegas", state: "WY", zip: "12345")
     Shelter.create!(name: "Dog City", address: "1923 Dog Ln", city: "Doggington", state: "CO", zip: "80414")
     new_info = {name: "Cool Dogs Palace", address: "89123 Cool St", city: "Cool City", state: "Coolorado", zip: "88888"}

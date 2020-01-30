4ee81cda078303eb405c271750f93456c9647c0a
David Atkins <atki1080@gmail.com>
2020-01-30 14:56:24 -0700

    Combine tests

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -1,39 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "from the shelter show page" do
-  it "I can click 'Update Shelter' link on the show page" do
+  it "I can click update shelter, fill the update form, and click submit" do
     shelter_1 = Shelter.create!(name: "Shelter A", address: "123 Fake St", city: "Las Vegas", state: "WY", zip: "12345")
-    Shelter.create!(name: "Dog City", address: "1923 Dog Ln", city: "Doggington", state: "CO", zip: "80414")
+    new_info = {name: "Dog City", address: "1923 Dog Ln", city: "Doggington", state: "CO", zip: "80414"}
 
     visit "/shelters/#{shelter_1.id}"
     click_link "Update Shelter"
 
     expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
 
-    expect(page).to have_content("Update shelter")
-    expect(page).to have_content('Name')
-    expect(page).to have_content('Address')
-    expect(page).to have_content('City')
-    expect(page).to have_content('State')
-    expect(page).to have_content('Zip')
-  end
-
-  it "I can fill the update form and click submit" do
-    shelter_1 = Shelter.create!(name: "Shelter A", address: "123 Fake St", city: "Las Vegas", state: "WY", zip: "12345")
-    new_info = {name: "Dog City", address: "1923 Dog Ln", city: "Doggington", state: "CO", zip: "80414"}
-
-    visit "/shelters/#{shelter_1.id}/edit"
-
-    fill_in 'shelter[name]', with: new_info[:name]
-    fill_in 'shelter[address]', with: new_info[:address]
-    fill_in 'shelter[city]', with: new_info[:city]
-    fill_in 'shelter[state]', with: new_info[:state]
-    fill_in 'shelter[zip]', with: new_info[:zip]
+    fill_in 'name', with: new_info[:name]
+    fill_in 'address', with: new_info[:address]
+    fill_in 'city', with: new_info[:city]
+    fill_in 'state', with: new_info[:state]
+    fill_in 'zip', with: new_info[:zip]
 
     click_button("Submit")
 
     expect(current_path).to eq("/shelters/#{shelter_1.id}")
-
     expect(page).to have_content("Name: #{new_info[:name]}")
     expect(page).to have_content("Address: #{new_info[:address]}")
     expect(page).to have_content("City: #{new_info[:city]}")

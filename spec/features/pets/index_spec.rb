dbd42f1f9849f6a3fc69da0bc3b532643267f065
David Atkins <atki1080@gmail.com>
2020-01-31 08:39:11 -0700

    Add delete pet link

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -69,4 +69,35 @@ RSpec.describe "from the pets index page" do
 
     expect(current_path).to eq("/pets/#{pet_1.id}/edit")
   end
+  it "I can delete a pet" do
+    dog_city = Shelter.create!(
+      name: "Dog City",
+      address: "1923 Dog Ln",
+      city: "Doggington",
+      state: "CO",
+      zip: "80414")
+    pet_1 = Pet.create(
+      image_path: "https://i.pinimg.com/originals/a9/cf/64/a9cf6473ca327409108ab02d15cc06b0.jpg",
+      name: "Snoopy",
+      description: "beagle pup eh",
+      approximate_age: "6 months old",
+      sex: "male",
+      shelter: dog_city,
+      status: "adoptable")
+    Pet.create(
+      image_path: "https://upload.wikimedia.org/wikipedia/commons/2/2b/WelshCorgi.jpeg",
+      name: "Nana",
+      description: "super cute dog in need of home",
+      approximate_age: "4 years old",
+      sex: "female",
+      shelter: dog_city,
+      status: "adoptable")
+
+    visit '/pets'
+
+    click_link("Delete Pet", match: :first)
+
+    expect(current_path).to eq("/pets")
+    expect(page).to_not have_content(pet_1.name)
+  end
 end

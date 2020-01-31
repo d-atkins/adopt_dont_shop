1864035d6b7853604d94b276a1a1384a432a4e99
David Atkins <atki1080@gmail.com>
2020-01-31 07:33:56 -0700

    Add link to edit page from shelters index

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe "from the shelters index page" do
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
   end
+  it "I can get to the edit page of a shelter" do
+    shelter_1 = Shelter.create!(
+      name: "Shelter A",
+      address: "123 Fake St",
+      city: "Las Vegas",
+      state: "WY",
+      zip: "12345")
+    Shelter.create!(
+      name: "Dog City",
+      address: "1923 Dog Ln",
+      city: "Doggington",
+      state: "CO",
+      zip: "80414")
+
+    visit "/shelters"
+
+    click_link("Update Shelter", match: :first)
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+  end
 end

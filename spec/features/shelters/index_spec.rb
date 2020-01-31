e12848932c8682207a413e1b322f7b454ea445a6
David Atkins <atki1080@gmail.com>
2020-01-31 07:55:27 -0700

    Add link to delete shelter from shelters index

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -40,4 +40,33 @@ RSpec.describe "from the shelters index page" do
 
     expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
   end
+  it "I can delete a shelter" do
+    shelter_1 = Shelter.create!(
+      name: "Shelter A",
+      address: "123 Fake St",
+      city: "Las Vegas",
+      state: "WY",
+      zip: "12345")
+    Shelter.create!(
+      name: "Dog City",
+      address: "1923 Dog Ln",
+      city: "Doggington",
+      state: "CO",
+      zip: "80414")
+    Pet.create(
+      image_path: "https://i.pinimg.com/originals/a9/cf/64/a9cf6473ca327409108ab02d15cc06b0.jpg",
+      name: "Snoopy",
+      description: "beagle pup eh",
+      approximate_age: "6 months old",
+      sex: "male",
+      shelter: shelter_1,
+      status: "adoptable")
+
+    visit "/shelters"
+
+    click_link("Delete Shelter", match: :first)
+
+    expect(current_path).to eq("/shelters")
+    expect(page).to_not have_content(shelter_1.name)
+  end
 end

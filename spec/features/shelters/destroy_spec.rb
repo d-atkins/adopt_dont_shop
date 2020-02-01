4459dcf657976801f1263824030e7f7306c93b5d
David Atkins <atki1080@gmail.com>
2020-02-01 16:10:11 -0700

    Change to string interpolation

--- spec/features/shelters/destroy_spec.rb
+++ spec/features/shelters/destroy_spec.rb
@@ -20,8 +20,8 @@ RSpec.describe "As a visitor: " do
       click_link("Delete Shelter")
 
       expect(current_path).to eq('/shelters')
-      expect(page).to have_content('Name: ' + shelter_1.name)
-      expect(page).to_not have_content('Name: ' + shelter_2.name)
+      expect(page).to have_content("Name: #{shelter_1.name}")
+      expect(page).to_not have_content(shelter_2.name)
     end
   end
 end

8c51b8b81a6fb7684549415492d68ff4eb057f91
David Atkins <atki1080@gmail.com>
2020-02-01 16:08:14 -0700

    Make variable name more descriptive

--- spec/features/shelters/pets/new_spec.rb
+++ spec/features/shelters/pets/new_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor: " do
     describe 'from the shelter pets index page' do
-    it 'I can get to the new pet page, fill the form, and submit it' do
+    it 'I can get to the new pet page, fill out the form, and submit it' do
       dog_city = Shelter.create!(
         name: "Dog City",
         address: "1923 Dog Ln",
@@ -15,17 +15,8 @@ RSpec.describe "As a visitor: " do
         description: "beagle pup eh",
         approximate_age: "6 months old",
         sex: "male",
-        shelter: dog_city,
-        status: "adoptable")
-      pet_2 = Pet.create!(
-        image_path: "https://upload.wikimedia.org/wikipedia/commons/2/2b/WelshCorgi.jpeg",
-        name: "Nana",
-        description: "super cute dog in need of home",
-        approximate_age: "4 years old",
-        sex: "female",
-        shelter: dog_city,
-        status: "adoptable")
-      new_info = {
+        shelter: dog_city)
+      new_pet_info = {
         image_path: "https://cdn.mos.cms.futurecdn.net/g8PyY6xAhcndpQLLSkdPf-320-80.jpg",
         name: "Capy'n Hook",
         description: "dread of the seven seas",
@@ -37,12 +28,11 @@ RSpec.describe "As a visitor: " do
 
       expect(current_path).to eq("/shelters/#{dog_city.id}/pets/new")
 
-      fill_in 'image_path', with: new_info[:image_path]
-      fill_in 'name', with: new_info[:name]
-      fill_in 'description', with: new_info[:description]
-      fill_in 'approximate_age', with: new_info[:approximate_age]
-      fill_in 'sex', with: new_info[:sex]
-
+      fill_in 'image_path', with: new_pet_info[:image_path]
+      fill_in 'name', with: new_pet_info[:name]
+      fill_in 'description', with: new_pet_info[:description]
+      fill_in 'approximate_age', with: new_pet_info[:approximate_age]
+      fill_in 'sex', with: new_pet_info[:sex]
       click_button('Submit')
 
       expect(current_path).to eq("/shelters/#{dog_city.id}/pets")
@@ -50,14 +40,11 @@ RSpec.describe "As a visitor: " do
       expect(page).to have_content(pet_1.name)
       expect(page).to have_content(pet_1.approximate_age)
       expect(page).to have_content(pet_1.sex)
-      expect(page).to have_css("img[src*='#{pet_2.image_path}']")
-      expect(page).to have_content(pet_2.name)
-      expect(page).to have_content(pet_2.approximate_age)
-      expect(page).to have_content(pet_2.sex)
-      expect(page).to have_css("img[src*='#{new_info[:image_path]}']")
-      expect(page).to have_content(new_info[:name])
-      expect(page).to have_content(new_info[:approximate_age])
-      expect(page).to have_content(new_info[:sex])
+      
+      expect(page).to have_css("img[src*='#{new_pet_info[:image_path]}']")
+      expect(page).to have_content(new_pet_info[:name])
+      expect(page).to have_content(new_pet_info[:approximate_age])
+      expect(page).to have_content(new_pet_info[:sex])
     end
   end
 end

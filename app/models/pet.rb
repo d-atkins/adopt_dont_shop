541ae722f22d870bbdc6958396fab2d1bc6a4ced
David Atkins <atki1080@gmail.com>
2020-02-02 00:17:22 -0700

    Add sort_by_status class method

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,8 @@
 class Pet < ApplicationRecord
   validates_presence_of :image, :name, :description, :approximate_age, :sex, :status
   belongs_to :shelter
+
+  def self.sort_by_status
+    Pet.order(:status)
+  end
 end
